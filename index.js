2444cbe4a16008629f38cb982dc20a09ec0e3840
Mark A Guza <guzamaguza@gmail.com>
2019-12-23 05:12:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //collects assigns each letter as a key and the number of occurrences as the value
-function binaryMatch(input){
+function buildHistogram(input){
   let sortedInput = input.sort();
   let histGen;
   sortedInput.forEach(letter => {
@@ -10,7 +10,7 @@ function binaryMatch(input){
 
 
 function canBuildNote(magazine, note){
-  let hist = binaryMatch(magazine);
+  let hist = buildHistogram(magazine);
   for(let i=0;i<note.length;i++){
     if (hist[note[i]] > 0){hist[note[i]]--}
     else {return false}
