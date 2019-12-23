8f2482940e211541760da4b16b1906e43117b74a
Mark A Guza <guzamaguza@gmail.com>
2019-12-23 05:14:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function buildHistogram(input){
 
 function canBuildNote(magazine, note){
   let hist = buildHistogram(magazine);
-  //iterate thru each letter of the note and compare with magazine histogram 
+  //iterate thru each letter of the note and compare with magazine histogram
   for(let i=0;i<note.length;i++){
     if (hist[note[i]] > 0){hist[note[i]]--}
     else {return false}
